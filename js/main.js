4cb8a64a69f26e33e2492d0d1652cf2240616bbd
cecilialjungquist <111697322+cecilialjungquist@users.noreply.github.com>
2023-02-16 13:15:21 +0100

    Shopping cart functions, updates for renderProduct, overlay styling and function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+// Strukturera upp koden
+// Fixa så ikonen i varukorgen försvinner när man har clearat localStorage
+// Fixa så ikonen är kvar om localStorage finns
+// Mobilanpassa sidan
+// man ska kunna klicka på carten när innehållet visas, då ska varukorgen försvinna igen
+// Om varukorgen är tom ska inte knappen visas
+
+let cart = [];
+
 const headerEl = document.querySelector('header');
 headerEl.innerHTML = `
     <section>
@@ -5,10 +14,41 @@ headerEl.innerHTML = `
         <h3>popcorn store</h3>
     </section>
     <nav>
+        <aside class="cart-indicator"></aside>
         <img src="img/icon-login.svg" alt="login icon">
-        <img src="img/icon-cart.svg" alt="cart icon">
+        <img class="cart-icon" src="img/icon-cart.svg" alt="cart icon">
     </nav>
 `;
+
+let cartIcon = document.querySelector('.cart-icon');
+cartIcon.addEventListener('click', () => {
+
+    let cartEl = document.createElement('article');
+    cartEl.classList.add('products-in-cart');
+    let localStorageCart = JSON.parse(localStorage.getItem('cart'));
+
+    if (localStorageCart) {
+        console.log(localStorageCart);
+        localStorageCart.forEach(product => {
+            let cartProductInfo = document.createElement('section');
+            cartProductInfo.innerHTML = `
+                <h3>${product.name}</h3>
+                <p>${product.amount * product.pricePerHekto}kr</p>
+            `;
+            cartEl.appendChild(cartProductInfo);
+        });
+    }
+
+    let paymentBtn = document.createElement('button');
+    paymentBtn.innerHTML = 'Send me my stuff!';
+    cartEl.appendChild(paymentBtn);
+    paymentBtn.addEventListener('click', () => {
+        localStorage.clear();
+        document.querySelector('body').removeChild(cartEl)
+    })
+
+    document.querySelector('body').appendChild(cartEl);
+});
 
 const mainEl = document.querySelector('main');
 
@@ -27,34 +67,23 @@ async function fetchData() {
     products.forEach(product => {
         renderProduct(product);
     });
-
-    openOverlay(products[0]);
 }
 
 
 function renderProduct(product) {
     let articleEl = document.createElement('article');
-    articleEl.classList.add(product.name.toLowerCase());
-
     let imgEl = document.createElement('img');
-    imgEl.setAttribute('src', `../img/${product.img}`);
-    articleEl.appendChild(imgEl);
-
     let productDesc = document.createElement('p');
-    productDesc.innerHTML = product.desc;
-    articleEl.appendChild(productDesc);
-    
     let productInfo = setProductInfo(product, true);
-    articleEl.appendChild(productInfo);
-    
-    // articleEl.addEventListener('click', () => {
-    //     if (articleEl.querySelector('.hide')) {
-    //         articleEl.querySelector('.hide').classList.remove('hide');
-    //     } else {
-    //         articleEl.querySelector('section').classList.add('hide');
-    //     }
-    // });
 
+    imgEl.setAttribute('src', `../img/${product.img}`);
+    articleEl.classList.add(product.name.toLowerCase());
+    
+    productDesc.innerHTML = product.desc;
+    
+    articleEl.appendChild(imgEl);
+    articleEl.appendChild(productDesc);
+    articleEl.appendChild(productInfo);
     mainEl.appendChild(articleEl);
 }
 
@@ -62,12 +91,10 @@ function renderProduct(product) {
 function setProductInfo(product, withBuyBtn) {
 
     let productInfo = document.createElement('section');
-    // productInfo.classList.add('hide');
 
     let characteristicsEl = document.createElement('ul');
     let characteristicsKeys = Object.keys(product.characteristics);
     let characteristicsValues = Object.values(product.characteristics);
-    console.log(characteristicsValues);
 
     for (let i = 0; i < characteristicsKeys.length; i++) {
         let liEl = document.createElement('li');
@@ -83,33 +110,107 @@ function setProductInfo(product, withBuyBtn) {
         buyBtn.innerHTML = `Buy for ${product.pricePerHekto}kr/hg!`;
     
         buyBtn.addEventListener('click', () => {
-            // openOverlay();
+            openOverlay(product);
         })
         productInfo.appendChild(buyBtn)
     }
 
-
     return productInfo;
 }
 
+
 function openOverlay(product) {
+    // Skapa alla element
     let overlay = document.createElement('section');
-    overlay.classList.add('overlay');
-
     let articleEl = document.createElement('article');
-    
-    let input = document.createElement('input');
-    input.setAttribute('id', `${product.name}`);
-    articleEl.appendChild(input);
-
+    let productName = document.createElement('h2');
     let productInfo = setProductInfo(product, false);
+    let amount = 1;
+    let input = document.createElement('section');
+    let addToCartBtn = document.createElement('button');
 
-    articleEl.appendChild(productInfo);
+
+    overlay.classList.add('overlay');
+    addToCartBtn.classList.add('add-to-cart-btn');
+
+    productName.innerHTML = product.name;
+    input.innerHTML = `
+        <button class="plus">+</button>
+        <h4><span>${amount}</span>hg</h4>
+        <button class="minus">-</button>
+    `;
+    addToCartBtn.innerHTML = `
+        Buy ${amount} hg for ${amount * parseInt(product.pricePerHekto)}kr!
+    `;
     
-    product.weight = input.value;
-    console.log(product.weight);
-    
+    // Lägger till alla element
+    articleEl.appendChild(productName)
+    articleEl.appendChild(productInfo)
+    articleEl.appendChild(input)
+    articleEl.appendChild(addToCartBtn)
     overlay.appendChild(articleEl)
     document.querySelector('body').appendChild(overlay);
+
+    // Eventlyssnare
+    articleEl.querySelector('.plus').addEventListener('click', () => {
+        amount = updateAmount(amount, '+');
+        addToCartBtn.innerHTML = `Buy ${amount} hg for ${amount * parseInt(product.pricePerHekto)}kr!`;
+        input.querySelector('h4 span').innerHTML = amount;
+    });
+
+    articleEl.querySelector('.minus').addEventListener('click', () => {
+        amount = updateAmount(amount, '-');
+        addToCartBtn.innerHTML = `Buy ${amount} hg for ${amount * parseInt(product.pricePerHekto)}kr!`;
+        input.querySelector('h4 span').innerHTML = amount;
+    });
+
+    addToCartBtn.addEventListener('click', () => {
+        addToCart(product, amount);
+        document.querySelector('body').removeChild(overlay);
+    })
+}
+
+function updateAmount(amount, operator) {
+    if (operator === '+' && amount < 10) {
+        amount++;
+    } else if (operator === '-' && amount > 1) {
+        amount--;
+    }
+    return amount;
+}
+
+function addToCart(product, amount) {
+    // Adderar indikator
+    let cartIndicator = document.querySelector('.cart-indicator');
+    cartIndicator.style.opacity = '.8';
+
+    // Lägger till animering
+    cartIndicator.classList.add('pop');
+
+    // Tar bort animering
+    setTimeout(() => {
+        cartIndicator.classList.remove('pop');
+    }, 2000);
+
+    // Sätter ny prop (amount) och lägger till i cart
+    product.amount = amount;
+
+    // Om produkten redan finns så ska den inte läggas till?
+    console.log(cart);
+    updateLocalStorage(product);
+}
+
+function updateLocalStorage(product) {
+    // Hämta localStorage
+    let localStorageCart = JSON.parse(localStorage.getItem('cart'));
     
+    // Om den finns, hämta och uppdatera
+    if (localStorageCart) {
+        localStorageCart.push(product)
+        localStorage.setItem('cart', JSON.stringify(localStorageCart));
+    } else {
+        // Annars skapa den med ny product
+        let newLocalStorageCart = [product];
+        localStorage.setItem('cart', JSON.stringify(newLocalStorageCart));
+    }
 }
